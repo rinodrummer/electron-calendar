e65af79efd9e2a9b69b0f85bcc202550518786ab
Gennaro Landolfi <code@rinodrummer.dev>
2024-04-12 08:45:30 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './style.css'
+import './style.css';
 
-import './demos/ipc'
+import './demos/ipc';
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
 createApp(App)
-  .mount('#app')
-  .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+    .mount('#app')
+    .$nextTick(() => {
+        postMessage({ payload: 'removeLoading' }, '*');
+    });
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+    import type { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
 }
 
 interface Window {
-  // expose in the `electron/preload/index.ts`
-  ipcRenderer: import('electron').IpcRenderer
+    // expose in the `electron/preload/index.ts`
+    ipcRenderer: import('electron').IpcRenderer;
 }
