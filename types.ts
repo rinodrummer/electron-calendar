02d681cf70f75a630756b9b51b182eed7ad15d38
Gennaro Landolfi <code@rinodrummer.dev>
2024-04-25 23:51:19 +0200

    Improved utility types

--- types.ts
+++ types.ts
@@ -1,6 +1,32 @@
 import { EventApi } from '@fullcalendar/core';
+export type CalendarView = 'dayGridMonth' | 'timeGridWeek' | 'timeGridDay' | 'list';
+
+export type DateCompatible = string | number;
+export type BaseCalendarEvent = Pick<EventApi, 'title' | 'allDay'>;
+
+export type Tables = {
+    categories: {
+        id: number,
+        name: string,
+        color: string,
+        created_at?: DateCompatible,
+        updated_at?: DateCompatible,
+    },
+    events: {
+        id: number,
+        title: string,
+        description?: string,
+        starts_at: DateCompatible,
+        ends_at: DateCompatible,
+        is_all_day?: boolean,
+        category_id?: number,
+        created_at?: DateCompatible,
+        updated_at?: DateCompatible,
+    }
+};
 
 export interface Category {
+export type Table<K extends keyof Tables> = Tables[K];
     id: number,
     name: string,
     color: string,
@@ -11,6 +37,8 @@ export interface ICalendarEvent extends EventApi {
     description?: string,
     categoryID?: Category['id'],
     category?: Category,
+    categoryID?: Tables['categories']['id'],
+    category?: Tables['categories'],
 }
 
 export type CalendarEvent = Partial<ICalendarEvent>;
