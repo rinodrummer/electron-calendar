c1245c51d11285b7bc9ca4acdeb2543b8c1acbfb
Gennaro Landolfi <code@rinodrummer.dev>
2024-05-08 22:41:02 +0200

    Made is_all_day non-nullable, added unique index

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -13,12 +13,13 @@ CREATE TABLE events (
     description TEXT,
     starts_at DATETIME NOT NULL,
     ends_at DATETIME NOT NULL,
-    is_all_day BOOLEAN DEFAULT FALSE,
+    is_all_day BOOLEAN NOT NULL DEFAULT FALSE,
     category_id INTEGER REFERENCES categories ON DELETE SET NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE UNIQUE INDEX events_unique ON events(title, starts_at, ends_at);
 CREATE INDEX event_dates ON events(starts_at, ends_at);
 
 -- Down
